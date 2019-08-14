025fd95fd439f6e837ed8cae942cf2a73bfe776c
Andrew Huntington <andrew.huntington@gmail.com>
2019-08-14 23:41:18 +0900

    Setting Up the Field
    -Added sprites for paddles and ball
    -Gave left paddle player controlled vertical movement

---
+++ Assets/Player_Input_Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Input_Controller : MonoBehaviour
+{
+    // Script that handles input from two players
+    // Player 1 => Controls left bat with W/S keys
+    // Player 2 => Controls right bat with arrow keys
+
+    [SerializeField] GameObject leftPaddle;
+    [SerializeField] GameObject rightPaddle;
+    [SerializeField] float paddleSpeed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftPaddle.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Move the bat up
+            leftPaddle.GetComponent<Rigidbody2D>().velocity = Vector2.up * paddleSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            //Move the bat down
+            leftPaddle.GetComponent<Rigidbody2D>().velocity = Vector2.down * paddleSpeed;
+        }
+    }
+}
